2ed0d2be1c48e8fcf3efc0e6873058c5ce8501c5
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-10-08 00:50:09 -0400

    init

---
+++ src/main/java/com/Exali/repulsorMod/RepulsorMod.java
@@ -0,0 +1,26 @@
+package com.Exali.repulsorMod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("repulsormod")
+public class RepulsorMod
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public RepulsorMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) { }
+
+    private void doClientStuff(final FMLClientSetupEvent event) { }
+}
